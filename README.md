3327de914e6382e1fa7bdc5397d1a8a8728120bf
AJ_Trimble <trimblea@grinnell.edu>
2024-10-01 09:12:39 -0500

    Changes Tally constructor to set to 0

--- README.md
+++ README.md
@@ -9,3 +9,9 @@ Authors:
 * Jafar Jarrar
 
 This code may be found at <FILL IN>.
+
+Prediction for exercise 2c:
+We expect that there is no difference when the class call is changed as it calls the superclass constructor.
+
+Prediction for 2f:
+All the values created with the Tally constructor will be set to 0. The Tally constructor will take no parameter.
--- src/main/java/edu/grinnell/csc207/experiments/CounterExperiments.java
+++ src/main/java/edu/grinnell/csc207/experiments/CounterExperiments.java
@@ -2,7 +2,7 @@ package edu.grinnell.csc207.experiments;
 
 import edu.grinnell.csc207.util.Counter;
 // import edu.grinnell.csc207.util.BasicCounter;
-
+import edu.grinnell.csc207.util.Tally;
 import java.io.PrintWriter;
 import edu.grinnell.csc207.util.BasicCounter;
 
@@ -21,7 +21,7 @@ public class CounterExperiments {
     PrintWriter pen = new PrintWriter(System.out, true);
 
     // Set up some counters
-    Counter alpha = new BasicCounter(0);
+    Counter alpha = new Tally();
     Counter beta = new BasicCounter(123);
     Counter gamma = new BasicCounter(-5);
 
